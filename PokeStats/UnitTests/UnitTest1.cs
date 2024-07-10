96aa5d73d43d6941bd216cccac14483a5684063e
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-07-10 00:23:36 -0500

    .

--- PokeStats/UnitTests/UnitTest1.cs
+++ PokeStats/UnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Infrastructure.Data;
+using Infrastructure.Enums;
+using Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Frameworks;
 using NUnit.Framework.Constraints;
@@ -17,6 +19,34 @@ namespace UnitTests
         {
             var pokeContext = new Infrastructure.Data.PokeContext(new DbContextOptions<PokeContext>());
             Assert.That(pokeContext.Database.CanConnect());
+        }
+
+        [Test]
+        public void CSVHelperTest()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Files", "pokemon.csv");
+            var readCsv = File.ReadAllLines(path);
+
+            foreach (var line in readCsv)
+            {
+                var item = line.Split(',');
+                var pokemon = new Pokemon
+                {
+                    PokemonId = int.Parse(item[0]),
+                    Name = item[1],
+                    Type1 = int.Parse(item[1]),
+                    Type2 = item[3],
+                    Total = int.Parse(item[4]),
+                    HealthPoints = int.Parse(item[5]),
+                    Attack = int.Parse(item[6]),
+                    Defense = int.Parse(item[7]),
+                    SpecialAttack = int.Parse(item[8]),
+                    SpecialDefense = int.Parse(item[9]),
+                    Speed = int.Parse(item[10]),
+                    Generation = int.Parse(item[11]),
+                    Program = bool.Parse(item[12])
+                };  
+            }
         }
     }
 }
